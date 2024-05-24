042e9fa6eb7d3bfcef1dc4cc13ba1c314a0ce23b
gds90 <you@example.com>
2024-05-24 14:57:06 +0200

    Creato un array con la lista dei post in file posts.js; Creata rotta /posts che ritorna la lista dei post in HTML e JSON

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { posts } = require("./controllers/posts.js");
+
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog</h1>');
 })
+
+app.get('/posts', posts);
 
 app.listen(port, () => {
     console.log(`Server in ascolto su http://localhost:${port}`);
