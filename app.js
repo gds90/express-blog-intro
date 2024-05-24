db2ef1ae775654cb8aa2e9bd34d77a864de33994
gds90 <you@example.com>
2024-05-24 16:35:19 +0200

    BONUS completato, chiamata POST funzionante

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { posts } = require("./controllers/posts.js");
+const posts = require("./controllers/posts");
 
+app.use(express.json());
+
+// Asset statico per la cartella public
 app.use(express.static('public'));
 
+// Rotte
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog</h1>');
 })
+app.get('/posts', posts.get);
+app.post('/posts', posts.post);
 
-app.get('/posts', posts);
-
+// Server in ascolto
 app.listen(port, () => {
     console.log(`Server in ascolto su http://localhost:${port}`);
 });
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,58 +1,24 @@
-const posts = [
-    {
-        title: "Come imparare a cucinare piatti italiani",
-        content: "La cucina italiana è famosa in tutto il mondo per i suoi sapori unici e autentici. In questo post, condividerò alcune delle mie ricette preferite e consigli su come preparare piatti italiani a casa.",
-        image: "cucina_italiana.jpg",
-        tags: ["cucina", "ricette", "Italia", "gastronomia"]
-    },
-    {
-        title: "Le migliori serie TV da non perdere",
-        content: "Ci sono così tante serie TV disponibili oggi che può essere difficile scegliere cosa guardare. In questo post, elencherò alcune delle migliori serie TV da non perdere, con una breve recensione di ciascuna.",
-        image: "serie_tv.jpg",
-        tags: ["serie TV", "intrattenimento", "recensioni", "consigli"]
-    },
-    {
-        title: "Viaggio attraverso le meraviglie dell'Italia",
-        content: "L'Italia è un paese ricco di storia, cultura e bellezze naturali. In questo post, vi guiderò attraverso alcune delle destinazioni più affascinanti d'Italia.",
-        image: "italia.jpg",
-        tags: ["viaggi", "Italia", "cultura", "turismo"]
-    },
-    {
-        title: "Come pianificare una vacanza perfetta",
-        content: "Pianificare una vacanza può essere stressante, ma con alcuni consigli è possibile organizzare un viaggio perfetto. In questo post, condividerò alcuni suggerimenti su come pianificare ogni dettaglio della tua vacanza.",
-        image: "vacanza.jpg",
-        tags: ["viaggi", "pianificazione", "vacanza", "consigli"]
-    },
-    {
-        title: "Il mio primo viaggio in Giappone",
-        content: "Il Giappone è un paese affascinante con una cultura ricca e diversificata. In questo post vi racconto la mia esperienza durante il mio primo viaggio in Giappone, esplorando Tokyo, Kyoto e altre città meravigliose.",
-        image: "giappone.jpg",
-        tags: ["viaggi", "Giappone", "cultura", "avventura"]
-    },
-    {
-        title: "I benefici dello yoga per il corpo e la mente",
-        content: "Lo yoga è una pratica millenaria che offre numerosi benefici per il corpo e la mente. In questo post, esploreremo i vari tipi di yoga e come possono migliorare il nostro benessere.",
-        image: "yoga.jpg",
-        tags: ["benessere", "yoga", "salute", "fitness"]
-    }
-]
+const posts = require('../posts.json')
+const { writeJSON, readJSON } = require("../functions.js");
 
 module.exports = {
-    posts: (req, res) => {
+    get: (req, res) => {
         res.format({
             html: () => {
                 let html = '<main>';
                 posts.forEach((post) => {
                     html += `
                     <article>
-                    <h2>${post.title}</h2>
-                    <img width="500" src="/${post.image}" alt="${post.title}">
-                    <p>${post.content}</p>
-                    <h4>Tags:</h4>
-                    <ul>
-                        ${post.tags.map(tag => `<li>${tag}</li>`).join('')}
-                    </ul>
-                    </article>`
+                        <h2>${post.title}</h2>
+                        <img width="500" src="/${post.image}" alt="${post.title}">
+                        <p>${post.content}</p>
+                        <h4>Tags:</h4>
+                        <ul>`;
+                    post.tags.forEach(tag => {
+                        html += `<li>${tag}</li>`;
+                    });
+                    html += `</ul>
+                    </article>`;
                 });
                 html += '</main>';
                 res.send(html);
@@ -65,5 +31,10 @@ module.exports = {
                 });
             }
         })
+    },
+    post: (req, res) => {
+        const posts = readJSON('posts');
+        writeJSON('posts', [...posts, req.body]);
+        res.send('Post aggiunto al file JSON');
     }
 }
